551f0b1900f93e070385572c28c4b174a679a844
Evgeny Goldin <evgenyg@gmail.com>
2011-06-07 15:29:09 +0300

    Constants implements ApplicationContextAware, InitializingBean

--- messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/Constants.groovy
+++ messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/Constants.groovy
@@ -2,42 +2,59 @@ package com.goldin.plugins.teamcity.messenger
 
 import com.intellij.openapi.diagnostic.Logger
 import jetbrains.buildServer.web.openapi.PluginDescriptor
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+
 
 /**
  * Various constants
  */
-class Constants
+class Constants implements ApplicationContextAware, InitializingBean
 {
     static final String PLUGINS_CATEGORY = 'com.goldin.plugins'
     static final Logger LOG              = Logger.getInstance( PLUGINS_CATEGORY )
 
     
     PluginDescriptor   descriptor
-    ApplicationContext context
     String             pluginName
+    ApplicationContext context
 
-    Constants ( PluginDescriptor descriptor, ApplicationContext context )
+    
+    Constants ( PluginDescriptor descriptor )
     {
         setDescriptor( descriptor )
-        setContext   ( context.parent )
         setPluginName( descriptor.pluginName )
+    }
 
+
+    @Override
+    void setApplicationContext ( ApplicationContext context )
+    {
+        setContext ( context )
+    }
+
+    
+    @Override
+    void afterPropertiesSet ()
+    {
         if ( LOG.isDebugEnabled())
         {
-            def beansCount = getContext().beanDefinitionCount
-            def beanNames  = getContext().beanDefinitionNames.sort()
-
-            assert beansCount == beanNames.size()
+            List<String> beanNames  = [ context, context.parent ]*.beanDefinitionNames.toList().flatten()
+            int          beansCount = beanNames.size()
+            assert       beanNames
 
             def digits     = Math.log10( beansCount ).next() as int
             def maxLength  = beanNames*.size().max()
             def counter    = 1
             def beans      = beanNames.inject( new StringBuilder()){
                 StringBuilder b, String beanName ->
-                String beanClass = getContext().getBean( beanName ).getClass().name
-                beanName         = "[$beanName]".padRight( maxLength + 3 )
-                b << " [${ String.valueOf( counter++ ).padLeft( digits, '0' ) }]$beanName = [$beanClass]\n"
+
+                String beanCounter     = '[' + "${ counter++ }".padLeft( digits, '0' ) + ']'
+                Class  beanClass       = context.getBean( beanName ).getClass()
+                String beanNameD       = "[$beanName]".padRight( maxLength + 3 )
+                String beanDescription = "[$beanClass.name]${ beanClass.interfaces ? ' implements [' + beanClass.interfaces*.name.join( '][' ) + ']' : '' }"
+                b << " $beanCounter$beanNameD = $beanDescription\n"
                 b
             }
 
