4e04a38d56c484c3cb3d8e1000b43fb6ed357910
Evgeny Goldin <evgenyg@gmail.com>
2011-06-07 05:09:50 +0300

    Constants dumps all Spring beans upon start

--- messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/Constants.groovy
+++ messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/Constants.groovy
@@ -1,17 +1,45 @@
 package com.goldin.plugins.teamcity.messenger
 
+import groovy.util.logging.Log
 import jetbrains.buildServer.web.openapi.PluginDescriptor
+import org.springframework.context.ApplicationContext
 
 /**
  * Various constants
  */
+@Log
 class Constants
 {
-    PluginDescriptor descriptor
+    PluginDescriptor   descriptor
+    ApplicationContext context
 
-    Constants ( PluginDescriptor descriptor )
+    Constants ( PluginDescriptor descriptor, ApplicationContext context )
     {
         setDescriptor( descriptor )
+        setContext( context.parent )
+
+        def beansCount = getContext().beanDefinitionCount
+        def beanNames  = getContext().beanDefinitionNames.sort()
+
+        assert beansCount == beanNames.size()
+
+        def digits     = Math.log10( beansCount ).next() as int
+        def maxLength  = beanNames*.size().max()
+        def counter    = 1
+        def beans      = beanNames.inject( new StringBuilder()){
+            StringBuilder b, String beanName ->
+            String beanClass = getContext().getBean( beanName ).getClass().name
+            beanName         = "[$beanName]".padRight( maxLength + 3 )
+            b << " [${ String.valueOf( counter++ ).padLeft( digits, '0' ) }]$beanName = [$beanClass]\n"
+            b
+        }
+
+        log.fine( """
+ Constants loaded:
+ Plugin name = [${ descriptor.pluginName }]
+ [$beansCount] Spring beans available:
+$beans
+"""     )
     }
 
     String getPluginName() { descriptor.pluginName }
