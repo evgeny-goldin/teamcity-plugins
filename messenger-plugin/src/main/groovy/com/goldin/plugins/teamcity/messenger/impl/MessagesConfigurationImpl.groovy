dc1c76e68bcf604d4de547196cd0cb8673d5c948
Evgeny Goldin <evgenyg@gmail.com>
2011-06-22 16:53:37 +0300

    MessagesConfigurationImpl fixes for reading config

--- messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/impl/MessagesConfigurationImpl.groovy
+++ messenger-plugin/src/main/groovy/com/goldin/plugins/teamcity/messenger/impl/MessagesConfigurationImpl.groovy
@@ -2,15 +2,26 @@ package com.goldin.plugins.teamcity.messenger.impl
 
 import com.goldin.plugins.teamcity.messenger.api.MessagesConfiguration
 import jetbrains.buildServer.serverSide.MainConfigProcessor
+import org.gcontracts.annotations.Ensures
 import org.gcontracts.annotations.Requires
 import org.jdom.Element
-import org.jdom.Text
+import com.goldin.plugins.teamcity.messenger.api.MessagesContext
+import org.gcontracts.annotations.Invariant
+
 
 /**
  * {@link MessagesConfiguration} implementation
  */
+@Invariant({ ( this.ajaxRequestInterval  > 0 ) &&
+             ( this.persistencyInterval  > 0 ) &&
+             ( this.messagesLimitPerUser > 0 ) &&
+             ( this.messageLengthLimit   > 0 ) &&
+             ( this.dateFormatPattern && this.timeFormatPattern ) })
 class MessagesConfigurationImpl implements MessagesConfiguration, MainConfigProcessor
 {
+    private final MessagesContext     context
+    private final Map<String, String> defaults
+
     boolean minify
     int     ajaxRequestInterval
     int     persistencyInterval
@@ -20,35 +31,86 @@ class MessagesConfigurationImpl implements MessagesConfiguration, MainConfigProc
     String  timeFormatPattern
 
 
+    @Requires({ paramName && ( config != null ) && defaults })
+    @Ensures({ result })
+    private String param ( String paramName, Map config = [:] )
+    {
+        config[ paramName ] ?: defaults[ paramName ]
+    }
+
+
+    MessagesConfigurationImpl ( MessagesContext context )
+    {
+        this.context              = context
+        this.defaults             = map( new XmlParser().parse( getClass().getResourceAsStream( '/default-config.xml' ))).asImmutable()
+        this.minify               = param( 'minify'               ) as boolean
+        this.ajaxRequestInterval  = param( 'ajaxRequestInterval'  ) as int
+        this.persistencyInterval  = param( 'persistencyInterval'  ) as int
+        this.messagesLimitPerUser = param( 'messagesLimitPerUser' ) as int
+        this.dateFormatPattern    = param( 'dateFormatPattern'    )
+        this.timeFormatPattern    = param( 'timeFormatPattern'    )
+    }
+
+
+    /**
+     * Converts {@link Node} or {@link Element} to {@code Map<String, String>}.
+     *
+     * @param o object to convert
+     * @return object's mapping
+     */
+    @Requires({ o })
+    @Ensures({ result != null })
+    private Map<String, String> map ( Object o )
+    {
+        if ( o instanceof Node )
+        {
+            (( Node ) o ).children().inject( [:] ){ Map m, Node childNode ->
+
+                String text = childNode.text().trim()
+                assert childNode.name() && text
+
+                m[ childNode.name() ] = text
+                m
+            }
+        }
+        else if ( o instanceof Element )
+        {
+            (( Element ) o ).children.inject( [:] ){ Map m, Element childElement ->
+
+                String text = childElement.text.trim()
+                assert childElement.name && text
+
+                m[ childElement.name ] = text
+                m
+            }
+        }
+        else {
+            assert false, "Conversion to Map for class [${ o.class.name }] is not supported"
+        }
+    }
+
+
     @Override
     @Requires({ root })
     void readFrom ( Element root )
     {
-        Node   defaults = new XmlParser().parse( getClass().getResourceAsStream( '/default-config.xml' ))
-        assert defaults
+        Element rootNode = root.getChild( context.pluginName )
 
-        def configMap = root.children.inject( [:] ){ Map m, Element node -> m[ node.name ] = node.textTrim; m }
-        def get       = { String paramName -> ( configMap[ paramName ] ?: defaults.get( paramName ).text()) }
-
-        minify               = get( 'minify'               ) as boolean
-        ajaxRequestInterval  = get( 'ajaxRequestInterval'  ) as int
-        persistencyInterval  = get( 'persistencyInterval'  ) as int
-        messagesLimitPerUser = get( 'messagesLimitPerUser' ) as int
-        dateFormatPattern    = get( 'dateFormatPattern'    )
-        timeFormatPattern    = get( 'timeFormatPattern'    )
+        if ( rootNode )
+        {
+            Map<String, String> config = map( rootNode )
+            this.minify                = param( 'minify',               config ) as boolean
+            this.ajaxRequestInterval   = param( 'ajaxRequestInterval',  config ) as int
+            this.persistencyInterval   = param( 'persistencyInterval',  config ) as int
+            this.messagesLimitPerUser  = param( 'messagesLimitPerUser', config ) as int
+            this.dateFormatPattern     = param( 'dateFormatPattern',    config )
+            this.timeFormatPattern     = param( 'timeFormatPattern',    config )
+        }
     }
 
 
     @Override
     void writeTo ( Element root )
     {
-        def  element = { String tagName, String tagValue -> new Element( tagName ).setContent( new Text( tagValue )) }
-        root.content = [ element( 'minify',               minify               as String ),
-                         element( 'ajaxRequestInterval',  ajaxRequestInterval  as String ),
-                         element( 'persistencyInterval',  persistencyInterval  as String ),
-                         element( 'messagesLimitPerUser', messagesLimitPerUser as String ),
-                         element( 'dateFormatPattern',    dateFormatPattern ),
-                         element( 'timeFormatPattern',    timeFormatPattern )]
     }
 }
-
