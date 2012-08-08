14b8b5148e75ba2811752591838a79278065dd71
Evgeny Goldin <evgenyg@gmail.com>
2012-08-08 18:02:35 +0200

    "report" plugin - WIP.

--- report/src/main/groovy/com/goldin/plugins/teamcity/report/ReportExtension.groovy
+++ report/src/main/groovy/com/goldin/plugins/teamcity/report/ReportExtension.groovy
@@ -2,16 +2,17 @@ package com.goldin.plugins.teamcity.report
 
 import jetbrains.buildServer.web.openapi.PagePlaces
 import jetbrains.buildServer.web.openapi.PlaceId
+import jetbrains.buildServer.web.openapi.PluginDescriptor
 import jetbrains.buildServer.web.openapi.SimplePageExtension
 
 import javax.servlet.http.HttpServletRequest
 
-
 class ReportExtension extends SimplePageExtension
 {
-    ReportExtension ( PagePlaces pagePlaces )
+    ReportExtension ( PagePlaces pagePlaces, PluginDescriptor descriptor )
     {
-        super( pagePlaces, PlaceId.ALL_PAGES_HEADER, 'reportPage', 'report.jsp' )
+        super( pagePlaces, PlaceId.ALL_PAGES_HEADER, descriptor.getParameterValue( 'name' ), 'report.jsp' )
+        register()
     }
 
 
