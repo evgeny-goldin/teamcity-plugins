ca1465cea6de337a2f0d21ef4439fa963ad1347b
Evgeny Goldin <evgenyg@gmail.com>
2012-08-11 16:03:25 +0200

    Console plugin - two tabs: Console / Context. Work in progress.

--- console/src/main/groovy/com/goldin/plugins/teamcity/console/ContextExtension.groovy
+++ console/src/main/groovy/com/goldin/plugins/teamcity/console/ContextExtension.groovy
@@ -1,9 +1,10 @@
 package com.goldin.plugins.teamcity.console
 
+import jetbrains.buildServer.web.openapi.CustomTab
 import jetbrains.buildServer.web.openapi.PagePlaces
 import jetbrains.buildServer.web.openapi.PlaceId
 import jetbrains.buildServer.web.openapi.PluginDescriptor
-import jetbrains.buildServer.web.openapi.SimpleCustomTab
+import jetbrains.buildServer.web.openapi.SimplePageExtension
 import jetbrains.buildServer.web.util.SessionUser
 
 import javax.servlet.http.HttpServletRequest
@@ -12,9 +13,15 @@ import javax.servlet.http.HttpServletRequest
 /**
  * Adds "Context" tab to Administration => Diagnostics.
  */
-class ContextExtension extends SimpleCustomTab
-{
+class ContextExtension extends SimplePageExtension implements CustomTab
+{   /**
+     * Extending SimpleCustomTab (as with ConsoleExtension) will not allow using a tab title ("consoleContext")
+     * that is different from plugin name ("console") - TeamCity throws
+     * ServletException: File "/plugins/consoleContext/displayContext.jsp" not found
+     */
+
     private final ContextReportHelper helper
+    private final PluginDescriptor    descriptor
 
 
     ContextExtension ( PagePlaces          pagePlaces,
@@ -22,11 +29,24 @@ class ContextExtension extends SimpleCustomTab
                        ContextReportHelper helper )
     {
         super( pagePlaces, PlaceId.ADMIN_SERVER_DIAGNOSTIC_TAB, descriptor.getParameterValue( 'name' ),
-               'displayContext.jsp', 'Context' )
+               'displayContext.jsp' )
 
-        this.helper = helper
+        this.helper     = helper
+        this.descriptor = descriptor
         register()
     }
+
+
+    @Override
+    String getTabId(){ descriptor.getParameterValue( 'name' ) + 'Context' }
+
+
+    @Override
+    String getTabTitle(){ 'Context' }
+
+
+    @Override
+    boolean isVisible (){ true }
 
 
     @Override
