7623809a9e1f23cc67b03c4a6f2d25897d9473a0
Evgeny Goldin <evgenyg@gmail.com>
2013-09-06 23:05:28 +0200

    CodeNarc fixes.

--- codenarc.groovy
+++ codenarc.groovy
@@ -5,21 +5,21 @@ ruleset {
 
     ruleset( "http://codenarc.sourceforge.net/StarterRuleSet-AllRulesByCategory.groovy.txt" ) {
 
-        DuplicateNumberLiteral   ( enabled : false )
-        DuplicateStringLiteral   ( enabled : false )
-        BracesForClass           ( enabled : false )
-        BracesForMethod          ( enabled : false )
-        BracesForIfElse          ( enabled : false )
-        BracesForForLoop         ( enabled : false )
-        BracesForTryCatchFinally ( enabled : false )
-        JavaIoPackageAccess      ( enabled : false )
-        VariableName             ( enabled : false )
-        FactoryMethodName        ( enabled : false )
-        SpaceBeforeOpeningBrace  ( enabled : false )
-        SpaceBeforeClosingBrace  ( enabled : false )
-        SpaceAfterIf             ( enabled : false )
-
-        LineLength               ( length  : 160   )
-        MethodName               ( regex   : /[a-z][\w\s'\(\)]*/ ) // Spock method names
+        DuplicateNumberLiteral          ( enabled : false )
+        DuplicateStringLiteral          ( enabled : false )
+        BracesForClass                  ( enabled : false )
+        BracesForMethod                 ( enabled : false )
+        BracesForIfElse                 ( enabled : false )
+        BracesForForLoop                ( enabled : false )
+        BracesForTryCatchFinally        ( enabled : false )
+        JavaIoPackageAccess             ( enabled : false )
+        VariableName                    ( enabled : false )
+        FactoryMethodName               ( enabled : false )
+        SpaceBeforeOpeningBrace         ( enabled : false )
+        SpaceBeforeClosingBrace         ( enabled : false )
+        SpaceAfterIf                    ( enabled : false )
+        ThisReferenceEscapesConstructor ( enabled : false )
+        LineLength                      ( length  : 160   )
+        MethodName                      ( regex   : /[a-z][\w\s'\(\)]*/ ) // Spock method names
     }
 }
